2b3a7f87a4435753df863e533fbe206e752fd469
David Cichy <37139851+DavidCichy@users.noreply.github.com>
2023-12-08 12:37:30 +0100

    Initial implementation of King chess piece.

---
+++ src/main/java/org/dc/cc/GameObjects/ChessPieces/King.java
@@ -0,0 +1,20 @@
+package org.dc.cc.GameObjects.ChessPieces;
+
+import org.dc.cc.GameObjects.Chessboard.Field;
+
+public class King extends ChessPiece implements IChessPiece {
+    ChessPieceType Type = ChessPieceType.KING;
+    public boolean isMoveValid(Field fromField, Field toField) {
+        if (toField.getRow() == fromField.getRow() && Math.abs(fromField.getColumn().ordinal() - toField.getColumn().ordinal()) == 1){
+            return true;
+        }
+        if (toField.getColumn() == fromField.getColumn() && Math.abs(fromField.getRow().ordinal() - toField.getRow().ordinal()) == 1){
+            return true;
+        }
+        if (Math.abs(fromField.getColumn().ordinal() - toField.getColumn().ordinal()) == 1 && Math.abs(fromField.getRow().ordinal() - toField.getRow().ordinal()) == 1){
+            return true;
+        }
+        return false;
+    }
+
+}
