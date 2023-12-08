8521f926cdc8db7cec194dba985e1f0cb9915054
David Cichy <37139851+DavidCichy@users.noreply.github.com>
2023-12-08 12:47:37 +0100

    'ChessPieceType' getter added to IChessPiece interface

--- src/main/java/org/dc/cc/GameObjects/ChessPieces/IChessPiece.java
+++ src/main/java/org/dc/cc/GameObjects/ChessPieces/IChessPiece.java
@@ -3,5 +3,6 @@ package org.dc.cc.GameObjects.ChessPieces;
 import org.dc.cc.GameObjects.Chessboard.Field;
 
 public interface IChessPiece {
+    public ChessPieceTypeEnum getChessPieceType();
     boolean isMoveValid(Field fromField, Field toField);
 }
