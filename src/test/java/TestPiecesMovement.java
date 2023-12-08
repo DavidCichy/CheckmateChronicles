e8a2020ea7927d153ca98d9d00901b79748c695d
David Cichy <37139851+DavidCichy@users.noreply.github.com>
2023-12-08 13:41:55 +0100

    TESTS 04: Major Refactor

---
+++ src/test/java/TestPiecesMovement.java
@@ -0,0 +1,40 @@
+import org.dc.cc.GameObjects.ChessPieces.King;
+import org.dc.cc.GameObjects.Chessboard.Board;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TestPiecesMovement {
+    Board board = new Board();
+
+    @Test
+    @DisplayName("King on Blank Board - Correct Movement")
+    public void testKingMovementCorrect(){
+        King king = new King();
+
+        Assertions.assertTrue(king.isMoveValid(board.getField("A", "1"), board.getField("A", "2")));
+        Assertions.assertTrue(king.isMoveValid(board.getField("A", "2"), board.getField("A", "1")));
+        Assertions.assertTrue(king.isMoveValid(board.getField("B", "1"), board.getField("B", "2")));
+        Assertions.assertTrue(king.isMoveValid(board.getField("B", "2"), board.getField("B", "1")));
+
+        Assertions.assertTrue(king.isMoveValid(board.getField("A", "1"), board.getField("B", "2")));
+        Assertions.assertTrue(king.isMoveValid(board.getField("B", "2"), board.getField("A", "1")));
+        Assertions.assertTrue(king.isMoveValid(board.getField("B", "1"), board.getField("A", "2")));
+        Assertions.assertTrue(king.isMoveValid(board.getField("A", "2"), board.getField("B", "1")));
+    }
+
+    @Test
+    @DisplayName("King on Blank Board - Incorrect Movement")
+    public void testKingMovementIncorrect(){
+        King king = new King();
+        Assertions.assertFalse(king.isMoveValid(board.getField("A","1"), board.getField("A", "3")));
+        Assertions.assertFalse(king.isMoveValid(board.getField("A","3"), board.getField("A", "1")));
+        Assertions.assertFalse(king.isMoveValid(board.getField("A","1"), board.getField("C", "1")));
+        Assertions.assertFalse(king.isMoveValid(board.getField("C","1"), board.getField("A", "1")));
+
+        Assertions.assertFalse(king.isMoveValid(board.getField("A","1"), board.getField("A", "8")));
+        Assertions.assertFalse(king.isMoveValid(board.getField("A","8"), board.getField("A", "1")));
+        Assertions.assertFalse(king.isMoveValid(board.getField("H","1"), board.getField("A", "1")));
+        Assertions.assertFalse(king.isMoveValid(board.getField("A","1"), board.getField("H", "1")));
+    }
+}
