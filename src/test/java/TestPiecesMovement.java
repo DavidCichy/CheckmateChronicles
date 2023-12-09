ae73c084979f053559c75b36a4c81903213f52bf
David Cichy <37139851+DavidCichy@users.noreply.github.com>
2023-12-09 08:26:16 +0100

    TESTS 07: Knight test. All tests for Chesspieces moved to interface obj.

--- src/test/java/TestPiecesMovement.java
+++ src/test/java/TestPiecesMovement.java
@@ -1,5 +1,4 @@
-import org.dc.cc.GameObjects.ChessPieces.King;
-import org.dc.cc.GameObjects.ChessPieces.Queen;
+import org.dc.cc.GameObjects.ChessPieces.*;
 import org.dc.cc.GameObjects.Chessboard.Board;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -11,32 +10,32 @@ public class TestPiecesMovement {
     @Test
     @DisplayName("King on Blank Board - Correct Movement")
     public void testKingMovementBlankBoardCorrect(){
-        King king = new King();
+        IChessPiece piece = new King();
 
-        Assertions.assertTrue(king.isMoveValid(board.getField("A", "1"), board.getField("A", "2")));
-        Assertions.assertTrue(king.isMoveValid(board.getField("A", "2"), board.getField("A", "1")));
-        Assertions.assertTrue(king.isMoveValid(board.getField("B", "1"), board.getField("B", "2")));
-        Assertions.assertTrue(king.isMoveValid(board.getField("B", "2"), board.getField("B", "1")));
+        Assertions.assertTrue(piece.isMoveValid(board.getField("A", "1"), board.getField("A", "2")));
+        Assertions.assertTrue(piece.isMoveValid(board.getField("A", "2"), board.getField("A", "1")));
+        Assertions.assertTrue(piece.isMoveValid(board.getField("B", "1"), board.getField("B", "2")));
+        Assertions.assertTrue(piece.isMoveValid(board.getField("B", "2"), board.getField("B", "1")));
 
-        Assertions.assertTrue(king.isMoveValid(board.getField("A", "1"), board.getField("B", "2")));
-        Assertions.assertTrue(king.isMoveValid(board.getField("B", "2"), board.getField("A", "1")));
-        Assertions.assertTrue(king.isMoveValid(board.getField("B", "1"), board.getField("A", "2")));
-        Assertions.assertTrue(king.isMoveValid(board.getField("A", "2"), board.getField("B", "1")));
+        Assertions.assertTrue(piece.isMoveValid(board.getField("A", "1"), board.getField("B", "2")));
+        Assertions.assertTrue(piece.isMoveValid(board.getField("B", "2"), board.getField("A", "1")));
+        Assertions.assertTrue(piece.isMoveValid(board.getField("B", "1"), board.getField("A", "2")));
+        Assertions.assertTrue(piece.isMoveValid(board.getField("A", "2"), board.getField("B", "1")));
     }
 
     @Test
     @DisplayName("King on Blank Board - Incorrect Movement")
     public void testKingMovementBlankBoardIncorrect(){
-        King king = new King();
-        Assertions.assertFalse(king.isMoveValid(board.getField("A","1"), board.getField("A", "3")));
-        Assertions.assertFalse(king.isMoveValid(board.getField("A","3"), board.getField("A", "1")));
-        Assertions.assertFalse(king.isMoveValid(board.getField("A","1"), board.getField("C", "1")));
-        Assertions.assertFalse(king.isMoveValid(board.getField("C","1"), board.getField("A", "1")));
+        IChessPiece piece = new King();
+        Assertions.assertFalse(piece.isMoveValid(board.getField("A","1"), board.getField("A", "3")));
+        Assertions.assertFalse(piece.isMoveValid(board.getField("A","3"), board.getField("A", "1")));
+        Assertions.assertFalse(piece.isMoveValid(board.getField("A","1"), board.getField("C", "1")));
+        Assertions.assertFalse(piece.isMoveValid(board.getField("C","1"), board.getField("A", "1")));
 
-        Assertions.assertFalse(king.isMoveValid(board.getField("A","1"), board.getField("A", "8")));
-        Assertions.assertFalse(king.isMoveValid(board.getField("A","8"), board.getField("A", "1")));
-        Assertions.assertFalse(king.isMoveValid(board.getField("H","1"), board.getField("A", "1")));
-        Assertions.assertFalse(king.isMoveValid(board.getField("A","1"), board.getField("H", "1")));
+        Assertions.assertFalse(piece.isMoveValid(board.getField("A","1"), board.getField("A", "8")));
+        Assertions.assertFalse(piece.isMoveValid(board.getField("A","8"), board.getField("A", "1")));
+        Assertions.assertFalse(piece.isMoveValid(board.getField("H","1"), board.getField("A", "1")));
+        Assertions.assertFalse(piece.isMoveValid(board.getField("A","1"), board.getField("H", "1")));
     }
 
 
@@ -44,27 +43,41 @@ public class TestPiecesMovement {
     @Test
     @DisplayName("Queen on Blank Board - Correct Movement - Horizontal and Vertical")
     public void testQueenMovementBlankBoardCorrect(){
-        Queen queen = new Queen();
-        Assertions.assertTrue(queen.isMoveValid(board.getField("A","1"), board.getField("A", "2")));
-        Assertions.assertTrue(queen.isMoveValid(board.getField("A","2"), board.getField("A", "1")));
-        Assertions.assertTrue(queen.isMoveValid(board.getField("A","1"), board.getField("A", "3")));
-        Assertions.assertTrue(queen.isMoveValid(board.getField("A","3"), board.getField("A", "1")));
-        Assertions.assertTrue(queen.isMoveValid(board.getField("A","1"), board.getField("A", "8")));
-        Assertions.assertTrue(queen.isMoveValid(board.getField("A","8"), board.getField("A", "1")));
+        IChessPiece piece = new Queen();
+        Assertions.assertTrue(piece.isMoveValid(board.getField("A","1"), board.getField("A", "2")));
+        Assertions.assertTrue(piece.isMoveValid(board.getField("A","2"), board.getField("A", "1")));
+        Assertions.assertTrue(piece.isMoveValid(board.getField("A","1"), board.getField("A", "3")));
+        Assertions.assertTrue(piece.isMoveValid(board.getField("A","3"), board.getField("A", "1")));
+        Assertions.assertTrue(piece.isMoveValid(board.getField("A","1"), board.getField("A", "8")));
+        Assertions.assertTrue(piece.isMoveValid(board.getField("A","8"), board.getField("A", "1")));
 
-        Assertions.assertTrue(queen.isMoveValid(board.getField("A","1"), board.getField("H", "1")));
-        Assertions.assertTrue(queen.isMoveValid(board.getField("H","1"), board.getField("A", "1")));
+        Assertions.assertTrue(piece.isMoveValid(board.getField("A","1"), board.getField("H", "1")));
+        Assertions.assertTrue(piece.isMoveValid(board.getField("H","1"), board.getField("A", "1")));
     }
     @Test
     @DisplayName("Queen on Blank Board - Correct Movement - Diagonal")
     public void testQueenMovementBlankBoardCorrectDiagonal(){
-        Queen queen = new Queen();
-        Assertions.assertTrue(queen.isMoveValid(board.getField("A1"), board.getField("c3")));
-        Assertions.assertTrue(queen.isMoveValid(board.getField("c3"), board.getField("a1")));
-        Assertions.assertTrue(queen.isMoveValid(board.getField("a1"), board.getField("h8")));
-        Assertions.assertTrue(queen.isMoveValid(board.getField("h8"), board.getField("a1")));
-        Assertions.assertTrue(queen.isMoveValid(board.getField("h1"), board.getField("a8")));
-        Assertions.assertTrue(queen.isMoveValid(board.getField("a8"), board.getField("h1")));
+        IChessPiece piece = new Queen();
+        Assertions.assertTrue(piece.isMoveValid(board.getField("A1"), board.getField("c3")));
+        Assertions.assertTrue(piece.isMoveValid(board.getField("c3"), board.getField("a1")));
+        Assertions.assertTrue(piece.isMoveValid(board.getField("a1"), board.getField("h8")));
+        Assertions.assertTrue(piece.isMoveValid(board.getField("h8"), board.getField("a1")));
+        Assertions.assertTrue(piece.isMoveValid(board.getField("h1"), board.getField("a8")));
+        Assertions.assertTrue(piece.isMoveValid(board.getField("a8"), board.getField("h1")));
     }
+    @Test
+    @DisplayName("Knight - Correct Movement")
+    public void testKnightMovementcorrect(){
+        IChessPiece piece = new Knight();
 
+        Assertions.assertTrue(piece.isMoveValid(board.getField("A1"), board.getField("C2")));
+        Assertions.assertTrue(piece.isMoveValid(board.getField("C2"), board.getField("A1")));
+        Assertions.assertTrue(piece.isMoveValid(board.getField("A1"), board.getField("B3")));
+        Assertions.assertTrue(piece.isMoveValid(board.getField("B3"), board.getField("A1")));
+
+        Assertions.assertTrue(piece.isMoveValid(board.getField("D4"), board.getField("E6")));
+        Assertions.assertTrue(piece.isMoveValid(board.getField("E6"), board.getField("D4")));
+        Assertions.assertTrue(piece.isMoveValid(board.getField("B", "1"), board.getField("A", "2")));
+        Assertions.assertTrue(piece.isMoveValid(board.getField("A", "2"), board.getField("B", "1")));
+    }
 }
