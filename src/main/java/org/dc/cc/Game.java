c110dd97b46783888edc3909beee37599896a820
David Cichy <37139851+DavidCichy@users.noreply.github.com>
2023-12-12 16:48:31 +0100

    Game file clear.

--- src/main/java/org/dc/cc/Game.java
+++ src/main/java/org/dc/cc/Game.java
@@ -1,18 +1,24 @@
 package org.dc.cc;
 
 import org.dc.cc.Engine.Engine;
+import org.dc.cc.Engine.Match;
+import org.dc.cc.GameObjects.ChessPieces.IChessPiece;
+import org.dc.cc.GameObjects.Players.Player;
+import org.dc.cc.Utilities.DataImporter;
 import org.dc.cc.Utilities.FieldMapper;
 import org.dc.cc.GameObjects.Chessboard.Board;
+import org.dc.cc.Utilities.JsonImporter;
+
+import java.util.ArrayList;
 
 public class Game {
+
     public Game() {
         runGame();
     }
 
     void runGame(){
-        Engine engine = new Engine();
-        engine.createBoard();
-        engine.showBoard();
+
 
     }
 }
